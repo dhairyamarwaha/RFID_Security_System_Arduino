6b6914327910e4c161c747015ce9570f8663a74a
Dhairya <68156888+dhairyamarwaha@users.noreply.github.com>
2021-06-10 11:30:00 +0530

    Update RFID_Security_system.ino

--- RFID_Security_system.ino
+++ RFID_Security_system.ino
@@ -1,8 +1,6 @@
 #include <SPI.h>
 #include <MFRC522.h>
 #include <Servo.h>
-#include <LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd(0x27,16,2);
  
 #define SS_PIN 10
 #define RST_PIN 9
@@ -17,7 +15,7 @@ void setup()
   Serial.begin(9600);   // Initiate a serial communication
   SPI.begin();      // Initiate  SPI bus
   mfrc522.PCD_Init();   // Initiate MFRC522
-  myServo.attach(6); //servo pin
+  myServo.attach(8); //servo pin
   myServo.write(0); //servo start position
   pinMode(LED_G, OUTPUT);
   pinMode(LED_R, OUTPUT);
@@ -25,13 +23,6 @@ void setup()
   noTone(BUZZER);
   Serial.println("Put your card to the reader...");
   Serial.println();
-  lcd.begin();
-  lcd.clear();         
-  lcd.backlight();
-  lcd.setCursor(0,0);
-  lcd.print("PLACE THE CARD");
-  lcd.setCursor(0,1);
-  lcd.print("ON THE SENSOR");
 
 }
 void loop() 
@@ -60,14 +51,9 @@ void loop()
   Serial.println();
   Serial.print("Message : ");
   content.toUpperCase();
-  if (content.substring(1) == "A6 BE 43 73") //change here the UID of the card/cards that you want to give access
+  if (content.substring(1) == "53 4C 8E 27") //change here the UID of the card/cards that you want to give access
   {
     Serial.println("Authorized access");
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("ACESS GRANTED");
-    lcd.setCursor(0,1);
-    lcd.print("CARD IDENTIFIED");
     Serial.println();
     delay(500);
     digitalWrite(LED_G, HIGH);
@@ -78,31 +64,14 @@ void loop()
     delay(5000);
     myServo.write(0);
     digitalWrite(LED_G, LOW);
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("PLACE THE CARD");
-    lcd.setCursor(0,1);
-    lcd.print("ON THE SENSOR");
   }
-
  
  else   {
-    Serial.println("ACCESS DENIED");
+    Serial.println(" Access denied");
     digitalWrite(LED_R, HIGH);
     tone(BUZZER, 300);
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("ACCESS DENIED");
-    lcd.setCursor(0,1);
-    lcd.print("USER NOT FOUND");
     delay(1000);
     digitalWrite(LED_R, LOW);
     noTone(BUZZER);
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("PLACE THE CARD");
-    lcd.setCursor(0,1);
-    lcd.print("ON THE SENSOR");
-    
   }
 } 
